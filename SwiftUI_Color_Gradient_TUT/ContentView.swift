c80b8fa781fdfffd95e3bbef5078f3c04f8256a4
SeonJungKim <ksj083895@gmail.com>
2021-02-08 18:58:23 +0500

    LinearGradient, RadialGradient, AngularGradient

--- SwiftUI_Color_Gradient_TUT/ContentView.swift
+++ SwiftUI_Color_Gradient_TUT/ContentView.swift
@@ -9,8 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            AngularGradient(gradient: Gradient(colors: [.red, .orange, .yellow, .green, .blue, .purple, .red]), center: .center)
+            VStack {
+                Text("AngularGradient")
+                    .font(.system(size: 50))
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+        
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing)
+            VStack {
+                Text("LinearGradient")
+                    .font(.system(size: 50))
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+        
+        ZStack {
+            RadialGradient(gradient: Gradient(colors: [Color.blue, Color.white]), center: .center, startRadius: 20, endRadius: 600)
+            VStack {
+                Text("RadialGradient")
+                    .font(.system(size: 50))
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+        
     }
 }
 
